2d747db6a8395cf28f18e5e5886e93e781e2c5e1
Lekuru <contact@lekuru.xyz>
2025-10-08 16:02:50 +0200

    Add requirements.txt for numba & numpy

--- setup.py
+++ setup.py
@@ -8,14 +8,19 @@ current_directory = os.path.dirname(os.path.abspath(__file__))
 with open(os.path.join(current_directory, "README.md"), "r") as f:
     long_description = f.read()
 
+with open(os.path.join(current_directory, "requirements.txt"), "r") as f:
+    requirements = f.read().splitlines()
+
 setuptools.setup(
     name="osz2",
     version=osz2.__version__,
     author=osz2.__author__,
     author_email=osz2.__email__,
+    install_requires=requirements,
     description="A python library for reading osz2 files",
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
     keywords=["osu", "osz2", "python", "bancho"],
+    python_requires=">=3.7",
 )
