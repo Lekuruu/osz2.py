f1c1b36beed911e53bfec38ec9203c84a3083d0a
Lekuru <contact@lekuru.xyz>
2025-10-08 12:34:09 +0200

    Add python package file

---
+++ osz2/__init__.py
@@ -0,0 +1,8 @@
+
+from .package import Osz2Package
+from .types import MetadataType
+from .file import File
+
+from .simple_cryptor import SimpleCryptor
+from .xxtea_reader import XXTEAReader
+from .xxtea import XXTEA
