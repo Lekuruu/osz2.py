7bd59cd956b814a90a7a4859af4ec8c8f6833ffb
Lekuru <contact@lekuru.xyz>
2025-10-08 12:55:48 +0200

    Add `setup.py` file

--- osz2/__init__.py
+++ osz2/__init__.py
@@ -1,4 +1,9 @@
 
+__author__ = "Lekuru"
+__email__ = "contact@lekuru.xyz"
+__version__ = "1.0.0"
+__license__ = "MIT"
+
 from .package import Osz2Package
 from .types import MetadataType
 from .file import File
